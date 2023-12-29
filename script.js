07e39603f14c1b0dd491e8e2c4acbe9ebd8d3b60
FatiatAdeB <adebayofatiat21@gmail.com>
2023-12-30 00:31:37 +0100

    initial commit

--- script.js
+++
@@ -1,7 +0,0 @@
-function openDialog() {
-    document.getElementById('overlay').style.display = 'flex';
-}
-
-function closeDialog() {
-    document.getElementById('overlay').style.display = 'none';
-}
