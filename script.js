ec1c3d7bfff7a6fecd47d7f24acf5527a78fa656
FatiatAdeB <adebayofatiat21@gmail.com>
2024-01-04 11:40:03 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+const navbarMenu = document.querySelector(".navbar .links");
+const iconMenu = document.querySelector('.icon-menu');
+const hideIconMenu = navbarMenu.querySelector('.iconclose');
+
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
+// show mobile menu
+iconMenu.addEventListener("click", () => {
+    navbarMenu.classList.toggle("show-menu");
+});
+
+// hide mobile menu
+hideIconMenu.addEventListener("click", () => iconMenu.click());
 
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
